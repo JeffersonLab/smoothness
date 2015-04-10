138ff0700520b17e82a5495c57bd4399de6a392e
ryans <ryans@jlab.org>
2015-04-10 10:32:25 -0400

    Updated shared js

--- web/resources/js/3d.js
+++ web/resources/js/3d.js
@@ -1,3 +1,71 @@
+/**
+ * Global String Enhancements
+ */
+if (typeof String.prototype.startsWith !== 'function') {
+    String.prototype.startsWith = function(str) {
+        return this.indexOf(str) === 0;
+    };
+}
+if (!String.prototype.encodeXml) {
+    String.prototype.encodeXml = function () {
+        return this.replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/'/g, '&apos;')
+        .replace(/"/g, '&quot;');
+    };
+}
+if (!String.prototype.decodeXml) {
+    String.prototype.decodeXml = function () {
+        return this.replace(/&quot;/g, '"')
+        .replace(/&apos;/g, '\'')
+        .replace(/&gt;/g, '>')
+        .replace(/&lt;/g, '<')
+        .replace(/&amp;/g, '&');
+    };
+}
+/**
+ * Common Namespace Declaration
+ */
+var jlab = jlab || {};
+/**
+ * Common Namespaced Functions
+ */
+//Function to ensure a AJAX request is only issued serially
+jlab.isRequest = function() {
+    return jlab.ajaxInProgress;
+};
+jlab.requestStart = function() {
+    jlab.ajaxInProgress = true;      
+};
+jlab.requestEnd = function() {
+    jlab.ajaxInProgress = false;   
+};
+//Display a piece of another page in a dialog
+jlab.openPageInDialog = function(href, title) {
+    $("<div class=\"page-dialog\"></div>")
+    .load(href + ' .dialog-content')
+    .dialog({
+        autoOpen: true,
+        title: title,
+        width: 840,
+        height: 590,
+        close: function() {
+            $(this).dialog('destroy').remove();    
+        }
+    });    
+};
+jlab.closePageDialogs = function() {
+    $(".page-dialog").dialog('destroy').remove();
+};
+// Format a string representation of an integer with commas
+jlab.integerWithCommas = function(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
+/**
+ * Common Event Handlers
+ */
+// Filter flyout events
 $(document).on("click", "#filter-flyout-link", function() {
     $("#filter-flyout-panel").slideDown();
     return false;
@@ -5,4 +73,27 @@ $(document).on("click", "#filter-flyout-link", function() {
 $(document).on("click", "#filter-flyout-close-button", function() {
     $("#filter-flyout-panel").slideUp();
     return false;
+});
+// Dialog events
+$(document).on("click", ".dialog-ready", function() {
+    var title = $(this).attr("data-dialog-title");    
+        
+    jlab.closePageDialogs();
+    jlab.openPageInDialog($(this).attr("href"), title);        
+    return false; 
+});    
+$(document).on("click", ".dialog-close-button", function() {
+    $(this).closest(".dialog").dialog("close");
+});
+// Prevent keypress from doing Full page HTTP POST in a dialog; instead programatically click dialog submit button
+$(document).on("keypress", ".dialog input", function(e) {
+    if(e.which === 13) {
+        $(this).closest("form").find(".dialog-submit-button").click();
+        
+        return false;
+    }
+});
+// Some form inputs can trigger submit on change
+$(document).on("change", ".change-submit", function() {
+    $(this).closest("form").submit();
 });
