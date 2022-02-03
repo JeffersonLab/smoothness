97ae128bc2a0757ae81c8fc33db4b627313262be
slominskir <ryan.slominski@gmail.com>
2022-02-03 18:53:16 -0500

    cleanup

--- docker/oracle/setup/03_default_data.sql
+++ docker/oracle/setup/03_default_data.sql
@@ -1,4 +1,5 @@
 alter session set container = XEPDB1;
 
--- Populate Movies
 insert into SMOOTHNESS_OWNER.MOIVE (MOVIE_ID, TITLE, RELEASE_DATE, DESCRIPTION, MPAA_RATING, DURATION_MINUTES) values (SMOOTHNESS_OWNER.MOVIE_ID.nextval, 'Ghostbusters', TO_DATE('1984/06/07', 'yyyy/mm/dd'), 'Three parapsychologists forced out of their university funding set up shop as a unique ghost removal service in New York City, attracting frightened yet skeptical customers.', 'PG', 105);
+
+insert into SMOOTHNESS_OWNER.STAFF (STAFF_ID, USERNAME, FIRSTNAME, LASTNAME) values (SMOOTHNESS_OWNER.STAFF_ID.nextval, 'tester', 'firstname', 'lastname');
