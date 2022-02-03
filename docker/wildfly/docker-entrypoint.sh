eff043537bce5cb4e58bbc23eefc9d1ed6a06f70
slominskir <ryan.slominski@gmail.com>
2022-02-03 12:42:16 -0500

    Setting up Docker

--- docker/wildfly/docker-entrypoint.sh
+++ docker/wildfly/docker-entrypoint.sh
@@ -7,7 +7,7 @@ if [[ -z ${SKIP_DB_WAIT} ]]; then
   echo "-------------------------------------------------"
 
   until java -cp /:/opt/jboss/wildfly/modules/com/oracle/database/jdbc/main/ojdbc11-21.3.0.0.jar \
-        /TestOracleConnection.java "jdbc:oracle:thin:BAM_OWNER/password@oracle:1521/xepdb1"
+        /TestOracleConnection.java "jdbc:oracle:thin:SMOOTHNESS_OWNER/password@oracle:1521/xepdb1"
   do
     echo -e $(date) " Still waiting for Oracle to start..."
     sleep 5
