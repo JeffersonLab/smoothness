958d69941aa24950d49afc1c6faf362200faef1f
slominskir <ryan.slominski@gmail.com>
2022-02-03 12:07:44 -0500

    Setting up Docker

---
+++ docker/oracle/setup/01_users.sql
@@ -0,0 +1,16 @@
+alter session set container = XEPDB1;
+
+ALTER SYSTEM SET db_create_file_dest = '/opt/oracle/oradata';
+
+create tablespace BAM;
+
+create user "BAM_OWNER" profile "DEFAULT" identified by "password" default tablespace "BAM" account unlock;
+
+grant connect to BAM_OWNER;
+grant unlimited tablespace to BAM_OWNER;
+
+grant create view to BAM_OWNER;
+grant create sequence to BAM_OWNER;
+grant create table to BAM_OWNER;
+grant create procedure to BAM_OWNER;
+grant create type to BAM_OWNER;
