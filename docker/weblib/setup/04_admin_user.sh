11fb45ef1f8d575e4f294283d64536e96a0db33a
ryans <ryans@jlab.org>
2022-03-14 12:26:31 -0400

    Wildfly User config

--- docker/weblib/setup/04_admin_user.sh
+++ docker/weblib/setup/04_admin_user.sh
@@ -3,4 +3,19 @@
 echo "----------------------------"
 echo "| Setup IV: Add Admin User |"
 echo "----------------------------"
-/opt/jboss/wildfly/bin/add-user.sh admin admin
+
+config_admin_user() {
+if [[ -z "${WILDFLY_USER}" ]]; then
+    echo "Skipping Wildfly admin user Setup: Must provide WILDFLY_USER in environment"
+    return 0
+fi
+
+if [[ -z "${WILDFLY_PASSWORD}" ]]; then
+    echo "Skipping Wildfly admin user Setup: Must provide WILDFLY_PASSWORD in environment"
+    return 0
+fi
+
+/opt/jboss/wildfly/bin/add-user.sh "${WILDFLY_USER}" "${WILDFLY_PASSWORD}"
+}
+
+config_admin_user
