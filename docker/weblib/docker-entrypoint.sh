daf2324d2fcf6e261393930688c3bfaae367575b
ryans <ryans@jlab.org>
2022-03-14 12:45:28 -0400

    Debug cleanup

--- docker/weblib/docker-entrypoint.sh
+++ docker/weblib/docker-entrypoint.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
+echo "----------------------------------------------------"
+echo "| Step 1: Waiting for Oracle DB to start listening |"
+echo "----------------------------------------------------"
 if [[ -n ${DB_USER} && -n ${DB_PASS} && -n ${ORACLE_SERVER} && -n ${DB_SERVICE} ]]; then
-  echo "----------------------------------------------------"
-  echo "| Step 1: Waiting for Oracle DB to start listening |"
-  echo "----------------------------------------------------"
-
   export ORACLE_DRIVER_PATH=/tmp/ojdbc11-21.5.0.0.jar
 
   curl -sS -o "${ORACLE_DRIVER_PATH}" https://repo1.maven.org/maven2/com/oracle/database/jdbc/ojdbc11/21.5.0.0/ojdbc11-21.5.0.0.jar
@@ -12,13 +11,13 @@ if [[ -n ${DB_USER} && -n ${DB_PASS} && -n ${ORACLE_SERVER} && -n ${DB_SERVICE} 
   until java -cp "/:${ORACLE_DRIVER_PATH}" \
         /TestOracleConnection.java "jdbc:oracle:thin:${DB_USER}/${DB_PASS}@${ORACLE_SERVER}/${DB_SERVICE}"
   do
-    echo -e $(date) " Still waiting for Oracle to start..."
+    echo $(date) " Still waiting for Oracle to start..."
     sleep 5
   done
 
-  echo -e $(date) " Oracle connection successful!"
+  echo $(date) " Oracle connection successful!"
 else
-  echo "Skipping DB Wait"
+  echo $(date) " Skipping DB Wait"
 fi
 
 echo "----------------------------"
@@ -29,11 +28,11 @@ echo "----------------------------"
 
 until curl http://localhost:8080 -sf -o /dev/null;
 do
-  echo -e $(date) " Still waiting for Wildfly to start..."
+  echo $(date) " Still waiting for Wildfly to start..."
   sleep 5
 done
 
-echo -e $(date) " Wildfly started!"
+echo $(date) " Wildfly started!"
 
 
 echo "---------------------------------"
