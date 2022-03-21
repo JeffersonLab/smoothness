d32ad3c61585acc05fd4c14ce64e09512f9b7bd4
ryans <ryans@jlab.org>
2022-03-21 10:58:27 -0400

    Conditionally reload after config

--- docker/weblib/setup/07_reload.sh
+++ docker/weblib/setup/07_reload.sh
@@ -4,4 +4,6 @@ echo "----------------------------"
 echo "| Setup VII: Reload Config |"
 echo "----------------------------"
 
-/opt/jboss/wildfly/bin/jboss-cli.sh -c --command=:reload
+if [[ -z "${SKIP_RELOAD_AFTER_CONFIG}" ]]; then
+  /opt/jboss/wildfly/bin/jboss-cli.sh -c --command=:reload
+fi
