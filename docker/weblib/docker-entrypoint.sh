450300ee93feb38ee68f1c0c41927bde204947f8
ryans <ryans@jlab.org>
2022-08-29 16:01:35 -0400

    Configure weblib container at build time [no ci]

--- docker/weblib/docker-entrypoint.sh
+++ docker/weblib/docker-entrypoint.sh
@@ -3,10 +3,7 @@
 echo "----------------------------------------------------"
 echo "| Step 1: Waiting for Oracle DB to start listening |"
 echo "----------------------------------------------------"
-if [[ -n ${ORACLE_USER} && -n ${ORACLE_PASS} && -n ${ORACLE_SERVER} && -n ${ORACLE_SERVICE} ]]; then
-  export ORACLE_DRIVER_PATH=/tmp/ojdbc11-21.5.0.0.jar
-
-  curl -sS -o "${ORACLE_DRIVER_PATH}" https://repo1.maven.org/maven2/com/oracle/database/jdbc/ojdbc11/21.5.0.0/ojdbc11-21.5.0.0.jar
+if [[ -n ${ORACLE_USER} && -n ${ORACLE_PASS} && -n ${ORACLE_SERVER} && -n ${ORACLE_SERVICE} && -n ${ORACLE_DRIVER_PATH} ]]; then
 
   until java -cp "/:${ORACLE_DRIVER_PATH}" \
         /TestOracleConnection.java "jdbc:oracle:thin:${ORACLE_USER}/${ORACLE_PASS}@${ORACLE_SERVER}/${ORACLE_SERVICE}" "${DB_DEBUG}"
@@ -33,17 +30,5 @@ do
 done
 
 echo $(date) " Wildfly started!"
-
-
-echo "---------------------------------"
-echo "| Step 3: Execute Setup Scripts |"
-echo "---------------------------------"
-
-SETUP_DIR=/setup
-
-for f in "${SETUP_DIR}"/*; do
-       echo "Executing ${f}"
-       "${f}"
-done
 
 sleep infinity
--- docker/weblib/setup/07_reload.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "----------------------------"
-echo "| Setup VII: Reload Config |"
-echo "----------------------------"
-
-if [[ -z "${SKIP_RELOAD_AFTER_CONFIG}" ]]; then
-  /opt/jboss/wildfly/bin/jboss-cli.sh -c --command=:reload
-fi
