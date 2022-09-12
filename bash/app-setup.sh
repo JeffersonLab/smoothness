801ab2ad8abe2481692fd9cfece17c24bdd86678
ryans <ryans@jlab.org>
2022-09-12 14:46:25 -0400

    updated script [no ci]

--- bash/app-setup.sh
+++ bash/app-setup.sh
@@ -1,14 +1,36 @@
 #!/bin/bash
 
-if [[ -z "${WILDFLY_HOME}" ]]; then
-    echo "Skipping Setup: Must provide WILDFLY_HOME in environment"
-    exit 0
+if [ ! -z "$1" && -f "$1" ]
+then
+echo "$1 exists, loading"
+. $1
 fi
 
-WILDFLY_CLI_PATH=${WILDFLY_HOME}/bin/jboss-cli.sh
+# Verify expected env set:
+while read var; do
+  [ -z "${!var}" ] && { echo "$var is not set. Exiting.."; exit 1; }
+done << EOF
+KEYCLOAK_REALM
+KEYCLOAK_RESOURCE
+KEYCLOAK_SECRET
+KEYCLOAK_SERVER_URL
+KEYCLOAK_WAR
+ORACLE_DATASOURCE
+ORACLE_PASS
+ORACLE_SERVER
+ORACLE_SERVICE
+ORACLE_USER
+WILDFLY_APP_HOME
+EOF
+
+# Optional params
+# - WILDFLY_SKIP_START
+# - WILDFLY_SKIP_STOP
+
+WILDFLY_CLI_PATH=${WILDFLY_APP_HOME}/bin/jboss-cli.sh
 
 wildfly_start_and_wait() {
-${WILDFLY_HOME}/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 &
+${WILDFLY_APP_HOME}/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 &
 
 until curl http://localhost:8080 -sf -o /dev/null;
 do
@@ -28,31 +50,6 @@ ${WILDFLY_CLI_PATH} -c shutdown
 }
 
 config_keycloak_client() {
-if [[ -z "${KEYCLOAK_WAR}" ]]; then
-    echo "Skipping Keycloak Client Setup: Must provide WAR in environment"
-    return 0
-fi
-
-if [[ -z "${KEYCLOAK_SERVER_URL}" ]]; then
-    echo "Skipping Keycloak Client Setup: Must provide KEYCLOAK_SERVER_URL in environment"
-    return 0
-fi
-
-if [[ -z "${KEYCLOAK_SECRET}" ]]; then
-    echo "Skipping Keycloak Client Setup: Must provide KEYCLOAK_SECRET in environment"
-    return 0
-fi
-
-if [[ -z "${KEYCLOAK_REALM}" ]]; then
-    echo "Skipping Keycloak Client Setup: Must provide KEYCLOAK_REALM in environment"
-    return 0
-fi
-
-if [[ -z "${KEYCLOAK_RESOURCE}" ]]; then
-    echo "Skipping Keycloak Client Setup: Must provide KEYCLOAK_RESOURCE in environment"
-    return 0
-fi
-
 DEPLOYMENT_CONFIG=principal-attribute="preferred_username",ssl-required=EXTERNAL,resource="${KEYCLOAK_RESOURCE}",realm="${KEYCLOAK_REALM}",auth-server-url=${KEYCLOAK_SERVER_URL}
 
 ${WILDFLY_CLI_PATH} -c <<EOF
@@ -64,31 +61,6 @@ EOF
 }
 
 config_oracle_client() {
-if [[ -z "${ORACLE_DATASOURCE}" ]]; then
-    echo "Skipping Oracle Client Setup: Must provide ORACLE_DATASOURCE in environment"
-    return 0
-fi
-
-if [[ -z "${ORACLE_SERVER}" ]]; then
-    echo "Skipping Oracle Client Setup: Must provide ORACLE_SERVER in environment"
-    return 0
-fi
-
-if [[ -z "${ORACLE_SERVICE}" ]]; then
-    echo "Skipping Oracle Client Setup: Must provide ORACLE_SERVICE in environment"
-    return 0
-fi
-
-if [[ -z "${ORACLE_USER}" ]]; then
-    echo "Skipping Oracle Client Setup: Must provide ORACLE_USER in environment"
-    return 0
-fi
-
-if [[ -z "${ORACLE_PASS}" ]]; then
-    echo "Skipping Oracle Client Setup: Must provide ORACLE_PASS in environment"
-    return 0
-fi
-
 ${WILDFLY_CLI_PATH} -c <<EOF
 batch
 data-source add --name=jdbc/${ORACLE_DATASOURCE} --driver-name=oracle --jndi-name=java:/jdbc/${ORACLE_DATASOURCE} --connection-url=jdbc:oracle:thin:@//${ORACLE_SERVER}/${ORACLE_SERVICE} --user-name=${ORACLE_USER} --password=${ORACLE_PASS} --max-pool-size=3 --min-pool-size=1 --flush-strategy=EntirePool --use-fast-fail=true --blocking-timeout-wait-millis=5000 --query-timeout=30 --idle-timeout-minutes=5 --background-validation=true --background-validation-millis=30000 --validate-on-match=false --check-valid-connection-sql="select 1 from dual" --prepared-statements-cache-size=10 --share-prepared-statements=true
