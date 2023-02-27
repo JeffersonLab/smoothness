5801faeb221ecc7a7873b7b4ed025bd74277d0dc
ryans <ryans@jlab.org>
2023-02-27 13:37:31 -0500

    javadoc [no ci]

--- smoothness-weblib/src/main/java/org/jlab/smoothness/business/service/CacheClearTimer.java
+++ smoothness-weblib/src/main/java/org/jlab/smoothness/business/service/CacheClearTimer.java
@@ -5,10 +5,17 @@ import javax.ejb.Singleton;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * Clear user authorization cache on a schedule.
+ */
 @Singleton
 public class CacheClearTimer {
     private static final Logger LOGGER = Logger.getLogger(
             CacheClearTimer.class.getName());
+
+    /**
+     * Scheduled method
+     */
     @Schedule(hour="0", minute="0", second="0", persistent=false, info="CacheClearTimer")
     public void scheduledCacheClear() {
         LOGGER.log(Level.FINE, "CacheClearTimer");
