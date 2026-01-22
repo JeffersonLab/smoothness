4f98b7785c66fe2c97f61b818883d27274d993c1
ryans <ryans@jlab.org>
2026-01-22 09:30:01 -0500

    Configurable CacheFilter
    Fixes #58

--- smoothness-weblib/src/main/java/org/jlab/smoothness/presentation/filter/CacheFilter.java
+++ smoothness-weblib/src/main/java/org/jlab/smoothness/presentation/filter/CacheFilter.java
@@ -14,7 +14,10 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServletResponseWrapper;
 
 /**
- * WebFilter for setting response cache directives.
+ * WebFilter for setting response cache directives. By default, will set maximum cache (1 year) for
+ * all responses with a mime type in the CACHEABLE_CONTENT_TYPES array. In a given servlet (request
+ * handler) the CACHEABLE_RESPONSE attribute can be used to override the default behavior and either
+ * forcibly set to OFF (no cache headers set) or MAX (set max cache headers of 1 year).
  *
  * @author ryans
  */
@@ -26,6 +29,17 @@ import javax.servlet.http.HttpServletResponseWrapper;
 public class CacheFilter implements Filter {
 
   private static final long EXPIRE_MILLIS = 31536000000L; // 365 days is max expires per spec
+
+  // Name of attribute to set on request processor to override default CachableResponse Auto
+  // behavior.
+  // Value should be CachableResponse enum
+  public final String CACHEABLE_RESPONSE = "CACHEABLE_RESPONSE";
+
+  public enum CachableResponse {
+    MAX, // Cache for spec max of 1 year
+    OFF, // Don't cache in filter, so maybe you can do your own cache logic
+    AUTO // Based on content type, will determine either OFF or MAX.  This is default.
+  }
 
   private static final String[] CACHEABLE_CONTENT_TYPES =
       new String[] {
@@ -51,7 +65,9 @@ public class CacheFilter implements Filter {
       throws IOException, ServletException {
     HttpServletResponse httpResponse = (HttpServletResponse) response;
 
-    chain.doFilter(request, new CacheControlResponse(httpResponse));
+    CachableResponse cachable = (CachableResponse) request.getAttribute(CACHEABLE_RESPONSE);
+
+    chain.doFilter(request, new CacheControlResponse(httpResponse, cachable));
   }
 
   @Override
@@ -62,25 +78,43 @@ public class CacheFilter implements Filter {
 
   class CacheControlResponse extends HttpServletResponseWrapper {
 
-    CacheControlResponse(HttpServletResponse response) {
+    private CachableResponse cachable;
+
+    CacheControlResponse(HttpServletResponse response, CachableResponse cachable) {
       super(response);
+      this.cachable = cachable;
     }
 
     @Override
     public void setContentType(String type) {
       super.setContentType(type);
 
-      if (type != null && Arrays.binarySearch(CACHEABLE_CONTENT_TYPES, type) > -1) {
-        super.setDateHeader("Expires", System.currentTimeMillis() + EXPIRE_MILLIS);
-        super.setHeader(
-            "Cache-Control", null); // Remove header automatically added by SSL/TLS container module
-        super.setHeader(
-            "Pragma", null); // Remove header automatically added by SSL/TLS container module
-      } else {
-        super.setHeader("Cache-Control", "no-store, no-cache, must-revalidate"); // HTTP 1.1
-        super.setHeader("Pragma", "no-cache"); // HTTP 1.0
-        super.setDateHeader("Expires", 0); // Proxies
+      if (cachable == null || cachable == CachableResponse.AUTO) {
+
+        if (type != null && Arrays.binarySearch(CACHEABLE_CONTENT_TYPES, type) > -1) {
+          setMaxCache();
+        } else {
+          setNoCache();
+        }
+      } else if (cachable == CachableResponse.MAX) {
+        setMaxCache();
+      } else { // OFF
+        setNoCache();
       }
+    }
+
+    private void setNoCache() {
+      super.setHeader("Cache-Control", "no-store, no-cache, must-revalidate"); // HTTP 1.1
+      super.setHeader("Pragma", "no-cache"); // HTTP 1.0
+      super.setDateHeader("Expires", 0); // Proxies
+    }
+
+    private void setMaxCache() {
+      super.setDateHeader("Expires", System.currentTimeMillis() + EXPIRE_MILLIS);
+      super.setHeader(
+          "Cache-Control", null); // Remove header automatically added by SSL/TLS container module
+      super.setHeader(
+          "Pragma", null); // Remove header automatically added by SSL/TLS container module
     }
   }
 }
